530c59e98482acab543f2619340bcf6b0ce6e31a
Jashanjot Singh <j.singh@brunel.net>
2019-02-20 09:52:01 +0100

    Add project files.

---
+++ CustomEffectTest/CustomEffectTest.Android/ChangeToolbarFontEffect.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel;
+using Android.Graphics;
+using Android.Widget;
+using CustomEffectTest.Droid;
+using CustomEffectTest.Effects;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ResolutionGroupName("CustomEffectTest")]
+[assembly: ExportEffect(typeof(ChangeToolbarFontEffect), "ChangeToolbarFontEffect")]
+namespace CustomEffectTest.Droid
+{
+    class ChangeToolbarFontEffect : PlatformEffect
+    {
+        TextView control;
+
+        protected override void OnAttached()
+        {
+            try
+            {
+                control = Control as TextView;
+                Typeface font = Typeface.CreateFromAsset(Forms.Context.ApplicationContext.Assets, ChangeFontEffect.GetFont(Element));
+                control.Typeface = font;
+            }
+            catch(Exception e)
+            {
+
+            }
+        }
+
+        protected override void OnDetached()
+        {
+            
+        }
+
+        protected override void OnElementPropertyChanged(PropertyChangedEventArgs args)
+        {
+            if (args.PropertyName == ChangeFontEffect.FontProperty.PropertyName)
+            {
+                var val = ChangeFontEffect.GetFont(Element);
+                if (val != null)
+                {
+                    Typeface font = Typeface.CreateFromAsset(Forms.Context.ApplicationContext.Assets, ChangeFontEffect.GetFont(Element));
+                    control.Typeface = font;
+                }
+            }
+        }
+    }
+}
---
+++ CustomEffectTest/CustomEffectTest/Effects/ChangeToolbarFontEffect.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using Xamarin.Forms;
+
+namespace CustomEffectTest.Effects
+{
+    public static class ChangeFontEffect
+    {
+        public static readonly BindableProperty FontProperty = BindableProperty.CreateAttached("Font",
+            typeof(string),
+            typeof(ChangeToolbarFontEffect),
+            null, propertyChanged: OnFontChanged);
+
+        private static void OnFontChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            if(bindable is Label labelControl)
+            {
+                if (!labelControl.Effects.Any((e) => e is ChangeToolbarFontEffect))
+                    labelControl.Effects.Add(new ChangeToolbarFontEffect());
+            }
+            else if(bindable is ToolbarItem toolbarItem)
+            {
+                if (bindable is ToolbarItem)
+                    if (!toolbarItem.Effects.Any((e) => e is ChangeToolbarFontEffect))
+                        toolbarItem.Effects.Add(new ChangeToolbarFontEffect());
+            }
+            return;
+        }
+
+        public static string GetFont(BindableObject view)
+        {
+            return (string)view.GetValue(FontProperty);
+        }
+
+        public static void SetFont(BindableObject view, string value)
+        {
+            view.SetValue(FontProperty, value);
+        }
+
+        class ChangeToolbarFontEffect : RoutingEffect
+        {
+            public ChangeToolbarFontEffect() : base("CustomEffectTest.ChangeToolbarFontEffect") { }
+        }
+    }
+}
